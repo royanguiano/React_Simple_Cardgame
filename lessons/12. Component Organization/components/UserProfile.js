60a55bdd39b2653904ebae2b88f9dcb8c8c2c8a2
Rodolfo Anguiano <roya@tuborrow.com>
2017-11-10 13:54:15 -0800

    updatin structure

--- lessons/12. Component Organization/components/UserProfile.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-let UserProfile = function() {
-  return <div>User Profile</div>
-}
-
-export default UserProfile
--- lessons/13. User Profile with React Router/README.md
+++
@@ -1,1 +0,0 @@
-Since this was the last video lesson in the series, the code for it is in the `/app` folder at the web root
